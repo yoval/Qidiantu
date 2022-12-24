a7f919c29ccc4ac5599ac228e556adf727f3d858
“Fuwenyue” <info@fuwenyue.com>
2022-12-24 18:37:58 +0800

    更新

--- Main.py
+++ Main.py
@@ -97,7 +97,7 @@ if __name__ == '__main__':
     allBookList = []
     year = 2022
     checkTagList = ['仙侠']
-    monthList = [1]
+    monthList = [3]
     for checkTag in checkTagList:
         pyName = p.get_pinyin(checkTag,'')
         mkdir(pyName)
